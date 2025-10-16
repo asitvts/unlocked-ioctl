fc347fa3bc732fe44a486b092a3510a621aa8ba6
asit vats <asitvts@gmail.com>
2025-10-16 15:42:44 +0530

    added signal handling

--- app.c
+++ app.c
@@ -4,20 +4,34 @@
 #include <unistd.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
 #define IOCTL_MAGIC 'k'
 #define IOCTL_CMD_GET _IOR(IOCTL_MAGIC,1,int)
 #define IOCTL_CMD_SET _IOW(IOCTL_MAGIC,2,int)
 #define IOCTL_CMD_MEMSET_NEGATIVE_ONE _IO(IOCTL_MAGIC,3)
+#define IOCTL_SEND_SIGNAL _IOW(IOCTL_MAGIC, 4, int)
+
+
+void sig_handler(int signum){
+	
+	if(signum==SIGUSR1){
+		printf("signal has been received: SIGUSR1, terminating\n");
+		exit(1);
+	}
+
+}
 
 
 int main(){
+
+	signal(SIGUSR1, sig_handler);
 
 	int fd = open("/dev/my_device", O_RDWR);
 	printf("fd=%d, errno=%d\n", fd, errno);
 	if(fd<0){
 		printf("error opening device: errno=%d\n", errno);
-		// Let's avoid strerror for now
 		return 1;
 	}
 
@@ -46,6 +60,17 @@ int main(){
 		printf("some kind of error in CMD_MEMSET_NEGATIVE_ONE\n");
 		return 4;
 	}
+	
+	int pid=(int)getpid();
+	ret = ioctl(fd, IOCTL_SEND_SIGNAL, &pid);
+	
+	
+	printf("sleeping\n");
+	sleep(10);
+	printf("sleep has ended\n");
+	
+	
+	
 	
 	close(fd);
 	
